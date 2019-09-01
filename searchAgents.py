7ae39db3173956459824449aa0767b53a949e517
kongpeter <xjtlu2014@outlook.com>
2019-09-01 16:36:13 +1000

    Update

--- searchAgents.py
+++ searchAgents.py
@@ -684,31 +684,84 @@ class CapsuleSearchProblem:
         return cost
 
 
+# def foodHeuristic(state, problem):
+#
+#     position, foodGrid, visitStatus = state
+#
+#     distance = []
+#     if visitStatus:
+#         capsule = problem.capsule
+#         prob = PositionSearchProblem(problem.startingGameState, start=position, goal=capsule, warn=False,
+#                                      visualize=False)
+#         distance.append(manhattanHeuristic(position, prob))
+#     else:
+#         foodList = foodGrid.asList()
+#         listLength = len(foodList)
+#
+#         if listLength == 0:
+#             return 0
+#         else:
+#             for food in foodList:
+#                 prob = PositionSearchProblem(problem.startingGameState, start=position, goal=food, warn=False,
+#                                          visualize=False)
+#                 distance.append(manhattanHeuristic(position, prob))
+#
+#     minLength = min(distance)
+#     maxLength = max(distance)
+#
+#     return 2 * maxLength
+
+
+
+    "*** YOUR CODE HERE ***"
+    #
+    # position, foodGrid = state
+    #
+    # result = 0
+    # distToDots = []
+    #
+    # for x in range(foodGrid.width):
+    #     for y in range(foodGrid.height):
+    #         if (foodGrid[x][y]):
+    #             distToDots.append(mazeDistance(position, (x, y), problem.startingGameState))
+    # if (not len(distToDots) == 0):
+    #     result = max(distToDots)
+    #     return result
+    # return result
+    #
+
+
+
+
+
+
+
 def foodHeuristic(state, problem):
 
     position, foodGrid, visitStatus = state
+    result = 0
+    distanceToFood = []
+    foodProblem = problem.startingGameState
 
-    distance = []
     if visitStatus:
-        capsule = problem.capsule
-        prob = PositionSearchProblem(problem.startingGameState, start=position, goal=capsule, warn=False,
+        prob = PositionSearchProblem(foodProblem, start=position, goal=problem.capsule, warn=False,
                                      visualize=False)
-        distance.append(manhattanHeuristic(position, prob))
+        distanceToFood.append(manhattanHeuristic(position, prob))
     else:
         foodList = foodGrid.asList()
         listLength = len(foodList)
 
         if listLength == 0:
-            return 0
+            return result
         else:
-            for food in foodList:
-                prob = PositionSearchProblem(problem.startingGameState, start=position, goal=food, warn=False,
-                                         visualize=False)
-                distance.append(manhattanHeuristic(position, prob))
+            for x in range(foodGrid.width):
+                for y in range(foodGrid.height):
+                    if (foodGrid[x][y]):
+                        distanceToFood.append(mazeDistance(position, (x, y), foodProblem))
 
-    minLength = min(distance)
-    maxLength = max(distance)
 
-    return 2 * maxLength
+        result = max(distanceToFood)
+
+    return result
 
 
